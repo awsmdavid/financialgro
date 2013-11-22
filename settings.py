0fc9871200084eb8f3e391bbbe8520f05e2575bc
awsmdavid <awsmdavid@gmail.com>
2013-11-21 23:21:37 -0500

    updated something..

--- settings.py
+++ settings.py
@@ -62,7 +62,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static')
+STATIC_ROOT = '/home4/davcheng/djangro/fingro/media/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -78,7 +78,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR, "fingro/static"),
+    '/home4/davcheng/djangro/fingro/static'
     #'/var/www/static/',
 )
 
