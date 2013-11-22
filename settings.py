7273c1b840883aa217d0b5e850fa0886bf025d02
awsmdavid <awsmdavid@gmail.com>
2013-11-21 23:46:06 -0500

    working version

--- settings.py
+++ settings.py
@@ -78,7 +78,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home4/davcheng/djangro/fingro/static'
+    os.path.join(BASE_DIR, "fingro/static"),
     #'/var/www/static/',
 )
 
